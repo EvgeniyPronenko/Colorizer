6d56d7ba7444b5757bf248b5a1fee00321da65a9
EvgeniyPronenko <pronenko.e@gmail.com>
2021-09-05 20:27:35 +0300

    Storyboard created

---
+++ Colorizer/ViewControllers/ColoriserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColoriserViewController.swift
+//  Colorizer
+//
+//  Created by Евгений on 05.09.2021.
+//
+
+import UIKit
+
+class ColoriserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
